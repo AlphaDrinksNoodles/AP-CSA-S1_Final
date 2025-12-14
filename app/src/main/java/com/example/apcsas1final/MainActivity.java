943a4c8cb0d8309174ef66ffee179bdc28df1788
Alpha <haaziqtahir@gmail.com>
2025-12-14 00:57:05 -0800

    More BaseUI Updates
    Finished the Title Screen and changed Orientation to Landscape.

--- app/src/main/java/com/example/apcsas1final/MainActivity.java
+++ app/src/main/java/com/example/apcsas1final/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apcsas1final;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,10 +11,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        mediaPlayer = MediaPlayer.create(this, R.raw.broken_promise_broken_dream_76_bpm_loop);
+
+        mediaPlayer.start();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
 
